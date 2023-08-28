0a1a1a25c676ecf1dc69ab1c3f7ba573dba82ecc
Ömer Bilici <102596640+omerblc@users.noreply.github.com>
2023-08-28 23:33:04 +0300

    Console.Write_and_Console.WriteLine
    I’ve learned the difference between Console.Write and Console.WriteLine. 😊

---
+++ Console.Write_and_Console.WriteLine/Console.Write_and_Console.WriteLine/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Congratulations!");
+Console.WriteLine(" ");
+Console.WriteLine("You wrote your first lines of code.");
+Console.WriteLine(" ");
+
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
