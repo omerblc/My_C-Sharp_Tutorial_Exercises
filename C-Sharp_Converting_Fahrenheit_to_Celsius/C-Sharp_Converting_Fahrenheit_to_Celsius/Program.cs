9cf694e207f0fd25176f442c63226f3151cb2ae7
Ömer Bilici <102596640+omerblc@users.noreply.github.com>
2023-09-04 17:27:26 +0300

    C-Sharp_Converting_Fahrenheit_to_Celsius
    I've made a Fahrenheit to Celsius converter.

---
+++ C-Sharp_Converting_Fahrenheit_to_Celsius/C-Sharp_Converting_Fahrenheit_to_Celsius/Program.cs
@@ -0,0 +1,8 @@
+﻿// C-Sharp_Converting_Fahrenheit_to_Celsius
+
+Random rnd = new Random();
+float fahrenheitDegree = rnd.Next(-10, 41);
+Console.WriteLine("Fahrenheit Degree: " + fahrenheitDegree);
+
+float celsiusResult = 100f/180f * (fahrenheitDegree - 32f);
+Console.WriteLine("Celsius Degree: " + celsiusResult);
